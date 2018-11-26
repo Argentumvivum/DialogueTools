18ec4aed2a06dd4b12790b47b6ce93daab67e491
Argentumvivum <40294754+Argentumvivum@users.noreply.github.com>
2018-11-26 14:03:31 +0100

    Added UTF-8 Encoding
    Changed encoding of a saved xml file.

--- DialogueSystemTool/Assets/Scripts/SavingSystem.cs
+++ DialogueSystemTool/Assets/Scripts/SavingSystem.cs
@@ -1,12 +1,15 @@
 ﻿using System.Xml.Serialization;
 using System.IO;
+using System.Text;
 
 public class SavingSystem
 {
     public static void SaveDialogue(string path, DialogueSystemContainer dsContainer)
     {
         var serializer = new XmlSerializer(typeof(DialogueSystemContainer));
-        using (var stream = new FileStream(path, FileMode.Create))
+        var encoding = Encoding.GetEncoding("UTF-8");
+
+        using (var stream = new StreamWriter(path, false, encoding))
         {
             serializer.Serialize(stream, dsContainer);
         }
